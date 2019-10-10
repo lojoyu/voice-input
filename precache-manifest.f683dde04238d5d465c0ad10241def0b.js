e91deef170ebe8e771c295e132a27e577dfac992
lojoyu <sophie200429@gmail.com>
2019-10-10 13:35:24 +0800

    Updates

---
+++ precache-manifest.f683dde04238d5d465c0ad10241def0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7c50faa58475eda637179ea22074da",
+    "url": "/voice-input/index.html"
+  },
+  {
+    "revision": "ad526690792e6f88a1ac",
+    "url": "/voice-input/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "28cf1c745ef03cba66d3",
+    "url": "/voice-input/static/js/2.2efc002c.chunk.js"
+  },
+  {
+    "revision": "ad526690792e6f88a1ac",
+    "url": "/voice-input/static/js/main.9728f64a.chunk.js"
+  },
+  {
+    "revision": "ca2d827ce8bc06b7f9a9",
+    "url": "/voice-input/static/js/runtime-main.92d4a780.js"
+  }
+]);
